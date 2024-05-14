920c6b55cf33108c3a37863d421a93b14c4b4394
Beanie <62057938+BeanieMen@users.noreply.github.com>
2024-05-14 10:46:06 +0530

    fix: add a mobile compat layout

--- app/page.tsx
+++ app/page.tsx
@@ -28,13 +28,37 @@ const items = [
   },
 ]
 
+const features = [
+  {
+    title: 'Quest-based assignments',
+    description:
+      'Complete quests to earn XP and gold, which you can use to buy gear and unlock new areas',
+    icon: <School />,
+  },
+  {
+    title: 'Personalized feedback',
+    description: 'Receive detailed feedback on your work to help you improve',
+    icon: <Puzzle />,
+  },
+  {
+    title: 'Reward system',
+    description: 'Earn badges and level up as you complete assignments',
+    icon: <ShieldCheck />,
+  },
+  {
+    title: 'Fun and engaging',
+    description:
+      'Quest is designed to make learning fun and engaging. You will feel like you are playing a game, not doing homework.',
+    icon: <Diamond />,
+  },
+]
+
 export default function Home() {
-  // const user = auth().userId
   let user
   if (user) redirect('/home')
   else {
     return (
-      <div className="bg-[#111622] min-h-screen">
+      <div className="bg-[#111622] min-h-screen px-5">
         <header className="w-full flex py-3 max-w-[100rem] mx-auto justify-between">
           <div className="flex items-center text-2xl">
             <BarChart /> Questify
@@ -51,81 +75,62 @@ export default function Home() {
         <div className="bg-[#253249] w-full h-[1px]"></div>
 
         <div className="max-w-6xl mx-auto">
-          {/* hero */}
-          <div
-            className="mt-5 mx-auto rounded-3xl grid grid-rows-2 bg-cover bg-center items-center h-[40.5rem] w-full"
-            style={{ backgroundImage: 'url("/hero.png")' }}
-          >
-            <div></div>
-            <div className="font-bold mx-5  gap-y-5 flex flex-col">
-              <p className="text-6xl">Homework, but make it a game</p>
-              <p className="text-lg font-light">
-                Questify is a gamified homework platform that makes learning
-                fun. We offer quest-based assignments, A reward <br />
-                system, and personalized feedback to help you level up your
-                academic skills
-              </p>
-              <div className="bg-blue-500 py-3 px-5 w-fit mt-5 rounded-full text-lg">
+          <div className="max-w-6xl mx-auto flex flex-col justify-center items-center h-screen">
+            <div
+              className="mt-5 rounded-3xl bg-cover bg-center flex flex-col items-center justify-center h-[40.5rem] w-full"
+              style={{ backgroundImage: 'url("/hero.png")' }}
+            >
+              <div className="font-bold mx-5 gap-y-5 flex flex-col text-center">
+                <p className="text-4xl sm:text-6xl">
+                  Homework, but make it a game
+                </p>
+                <p className="text-base sm:text-lg font-light">
+                  Questify is a gamified homework platform that makes learning
+                  fun. We offer quest-based assignments, a reward system, and
+                  personalized feedback to help you level up your academic
+                  skills
+                </p>
+                <div className="bg-blue-500 py-3 px-5 w-fit mt-5 rounded-full mx-auto text-lg">
+                  <SignUpButton>Get started</SignUpButton>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-20">
+            <p className="text-2xl sm:text-4xl font-bold text-center">
+              Why use Questify?
+            </p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-10">
+              {features.map((feature, index) => (
+                <div
+                  key={index}
+                  className="bg-[#1A2332] flex flex-col p-5 gap-y-2 border border-[#3f5272] rounded-lg"
+                >
+                  {feature.icon}
+                  <p className="font-bold text-lg">{feature.title}</p>
+                  <p className="text-md font-light">{feature.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="mt-20">
+            <p className="text-4xl font-bold text-center">
+              Ready to level up your homework game?
+            </p>
+            <div className="mt-10 mx-auto">
+              <div className="bg-blue-500 py-3 px-5 w-fit rounded-full mx-auto font-bold text-lg">
                 <SignUpButton>Get started</SignUpButton>
               </div>
             </div>
           </div>
 
-          {/* why use */}
-          <div className="flex mt-20 flex-col">
-            <p className="text-4xl font-bold">Why use Questify?</p>
-            <div className="grid grid-cols-4 gap-x-5 mt-20">
-              <div className="bg-[#1A2332] flex flex-col p-5 gap-y-2 border border-[#3f5272] rounded-lg">
-                <School />
-                <p className="font-bold text-lg">Quest-based assignments</p>
-                <p className="text-md font-light">
-                  Complete quests to earn XP and gold, which you can use to buy
-                  gear and unlock new areas
-                </p>
-              </div>
-              <div className="bg-[#1A2332] flex flex-col p-5 gap-y-2 border border-[#3f5272] rounded-lg">
-                <Puzzle />
-                <p className="font-bold text-lg">Personalized feedback</p>
-                <p className="text-md font-light">
-                  Recieve detailed feedback on your work to help you improve
-                </p>
-              </div>
-              <div className="bg-[#1A2332] flex flex-col p-5 gap-y-2 border border-[#3f5272] rounded-lg">
-                <ShieldCheck />
-                <p className="font-bold text-lg">Reward system</p>
-                <p className="text-md font-light">
-                  Earn badges and level up as you complete assignments
-                </p>
-              </div>
-              <div className="bg-[#1A2332] flex flex-col p-5 gap-y-2 border border-[#3f5272] rounded-lg">
-                <Diamond />
-                <p className="font-bold text-lg">Fun and engaging</p>
-                <p className="text-md font-light">
-                  Quest is desgined to make learning fun and engaging. You will
-                  feel like you are playing a game, not doing homework.
-                </p>
-              </div>
-            </div>
-
-            {/* ready */}
-            <div className="mt-40 flex flex-col justify-center">
-              <p className="text-5xl font-bold text-center">
-                Ready to level up your homework game?
-              </p>
-              <div className="mt-10 mx-auto">
-                <div className="bg-blue-500 py-3 px-5 w-fit rounded-full font-bold text-lg">
-                  <SignUpButton>Get started</SignUpButton>
-                </div>
-              </div>
-            </div>
-
-            {/* frequently */}
-            <div className="flex flex-col mt-40">
-              <span className="font-bold text-3xl mb-10">
-                Frequently asked questions
-              </span>
-              <Accordion items={items} />
-            </div>
+          <div className="mt-20">
+            <p className="text-3xl font-semibold mb-5">
+              Frequently asked questions
+            </p>
+            <Accordion items={items} />
           </div>
         </div>
       </div>
